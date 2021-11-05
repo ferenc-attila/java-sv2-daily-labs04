081285cfeb4e5967ab15556f2065bc8a425a34f4
Attila <attila.ferenc.80@gmail.com>
2021-11-05 16:45:30 +0100

    day03 Stock added solved by Kristof's advices

--- week04-labs-task/src/main/java/day03/Stock.java
+++ week04-labs-task/src/main/java/day03/Stock.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public class Stock {
 
-    List<Double> stock;
+    private List<Double> stock;
 
 
     public Stock(List<Double> stock) {
@@ -17,21 +17,29 @@ public class Stock {
         return stock;
     }
 
-    public double maxProfit () {
-        double maxProfit = 1;
-        for (int i = 0; i < this.stock.size()-1; i++) {
-            if (this.stock.get(i+1) > this.stock.get(i)) {
-                    maxProfit *= this.stock.get(i+1)/this.stock.get(i);
+    public double maxProfit() {
+        double maxProfit = 0;
+        for (int i = 0; i < this.stock.size(); i++) {
+            maxProfit = checkDailyDifferences(maxProfit, i);
+        }
+        return maxProfit;
+    }
+
+    private double checkDailyDifferences(double maxProfit, int i) {
+        for (int j = i + 1; j < this.stock.size(); j++) {
+            if (maxProfit < stock.get(j) - stock.get(i)) {
+                maxProfit = stock.get(j) - stock.get(i);
             }
         }
         return maxProfit;
     }
 
+
     public static void main(String[] args) {
-        Stock stock = new Stock(Arrays.asList(1.23,4.35,0.23,4.0,8.4));
+        Stock stock = new Stock(Arrays.asList(1.23, 4.35, 0.23, 4.0, 8.4));
         System.out.println(stock.maxProfit());  //8.17
 
-        Stock stock2 = new Stock(Arrays.asList(12.0,2.0,8.0,4.0));
+        Stock stock2 = new Stock(Arrays.asList(12.0, 2.0, 8.0, 4.0));
         System.out.println(stock2.maxProfit()); //6.0
     }
 }
